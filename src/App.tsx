952133c635708ea6c39c5832b15205e019149b66
abed obaah <ubahobaah@gmail.com>
2024-07-24 03:54:11 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,190 @@
+import imageAuth from "../src/assets/left-section.png"
+import email from "../src/assets/icon-mail.png"
+import { ArrowLeft2,ArrowRight2} from "iconsax-react"
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+
+
+
+export default function Example() {
+  return (
+    <div className="flex">
+      <img
+        alt=""
+        src={imageAuth}
+        className="h-full"
+      />
+      <div className="flex flex-col justify-between w-full">
+        <div className="flex items-center justify-between px-10">
+          <div className="flex items-center">
+            <ArrowLeft2 size="32" color="#757575" />
+            <h1 className="ml-2 text-[#757575]">WELCOME TO JCINSPECT</h1>
+          </div>
+          <div>
+            <p className=" text-[#424242}">Already a member? <span className="font-bold text-[#212121] underline">LOG IN NOW</span></p>
+          </div>
+        </div>
+
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Welcome to JCInspect
+          </h2>
+          <p className="mt-2 text-center text-xl font-normal leading-9 tracking-tight text-gray-900">SIGN IN TO  INSPECT</p>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
+            <div>
+              <div className="mt-2">
+              <Menu as="div" className="relative inline-block text-left">
+              <div>
+              <MenuButton style={{ width: '24rem' }} className="inline-flex justify-center gap-x-1.5 rounded-md bg-white py-5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                Options
+                <ChevronDownIcon aria-hidden="true" className="-mr-1 h-5 w-5 text-gray-400" />
+              </MenuButton>
+            </div>
+
+
+
+      <MenuItems
+        transition
+        className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+      >
+        <div className="py-1">
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+            >
+              Edit
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+            >
+              Duplicate
+            </a>
+          </MenuItem>
+        </div>
+        <div className="py-1">
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+            >
+              Archive
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+            >
+              Move
+            </a>
+          </MenuItem>
+        </div>
+        <div className="py-1">
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+            >
+              Share
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+            >
+              Add to favorites
+            </a>
+          </MenuItem>
+        </div>
+        <div className="py-1">
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+            >
+              Delete
+            </a>
+          </MenuItem>
+        </div>
+      </MenuItems>
+    </Menu>
+              </div>
+            </div>
+
+            <div>
+              <div className="mt-2">
+              <div className="flex items-center w-full rounded-md border border-gray-300 focus-within:border-gray-500 ">
+              <img
+              alt=""
+              src={email}
+              className="px-3"
+            />
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      placeholder="email@gmail.com"
+                      required
+                      className="block w-full py-5 pl-3 text-gray-900 placeholder:text-gray-400 border-0 sm:text-sm sm:leading-6"
+                    />
+                    
+                  </div>
+              </div>
+            </div>
+
+            <div>
+              <div className="mt-2">
+              <div className="flex items-center w-full rounded-md border border-gray-300 focus-within:border-gray-500 ">
+              <img
+              alt=""
+              src={email}
+              className="px-3"
+            />
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      placeholder="********"
+                      required
+                      autoComplete="current-password"
+                      className="block w-full py-5 pl-3 text-gray-900 placeholder:text-gray-400 border-0 sm:text-sm sm:leading-6"
+                    />
+                    
+                  </div>
+              </div>
+            </div>
+
+            <div>
+      <button
+        type="submit"
+        className="flex items-center justify-between w-full rounded-md bg-[#24243E] px-3 py-5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        <span>Sign in</span>
+        <ArrowRight2 size="24" className="ml-2 text-white" />
+      </button>
+    </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{' '}
+            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Start a 14 day free trial
+            </a>
+          </p>
+        </div>
+      </div>
+      </div>
+    </div>
+  )
+}
